024726750c9e8397319053d9c30d3db006bed3f6
tomoyanp <tomoyanpy@gmail.com>
2021-08-12 23:23:19 +0900

    m

--- webapp/go/src/isubata/app.go
+++ webapp/go/src/isubata/app.go
@@ -354,21 +354,21 @@ type MessageUser struct {
 }
 
 // TODO 直す
-func jsonifyMessage(m Message) (map[string]interface{}, error) {
-	u := User{}
-	err := db.Get(&u, "SELECT name, display_name, avatar_icon FROM user WHERE id = ?",
-		m.UserID)
-	if err != nil {
-		return nil, err
-	}
-
-	r := make(map[string]interface{})
-	r["id"] = m.ID
-	r["user"] = u
-	r["date"] = m.CreatedAt.Format("2006/01/02 15:04:05")
-	r["content"] = m.Content
-	return r, nil
-}
+// func jsonifyMessage(m Message) (map[string]interface{}, error) {
+// 	u := User{}
+// 	err := db.Get(&u, "SELECT name, display_name, avatar_icon FROM user WHERE id = ?",
+// 		m.UserID)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+//
+// 	r := make(map[string]interface{})
+// 	r["id"] = m.ID
+// 	r["user"] = u
+// 	r["date"] = m.CreatedAt.Format("2006/01/02 15:04:05")
+// 	r["content"] = m.Content
+// 	return r, nil
+// }
 
 func getMessage(c echo.Context) error {
 	userID := sessUserID(c)
